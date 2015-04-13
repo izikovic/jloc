8002c8ec6f2860e9b12a111543f4d1a81a05d195
James Booth <boothj5@gmail.com>
2015-04-14 00:05:20 +0100

    Added linecount test

--- src/test/java/com/boothj5/jloc/JlocTest.java
+++ src/test/java/com/boothj5/jloc/JlocTest.java
@@ -7,16 +7,26 @@ import static org.junit.Assert.assertEquals;
 public class JlocTest {
 
     @Test
-    public void fileCountShouldReturnTotalFilesRecursivelyForFilecount() {
+    public void fileCountFilecount() {
         Jloc jloc = new Jloc("/filecount");
 
         assertEquals(12, jloc.countFiles());
     }
 
     @Test
-    public void fileCountShouldReturnTotalFilesRecursivelyForChttpclient() {
+    public void fileCountChttpclient() {
         Jloc jloc = new Jloc("/chttpclient");
 
         assertEquals(24, jloc.countFiles());
+    }
+
+    @Test
+    public void lineCountChttpclient() {
+        Jloc jloc = new Jloc("/chttpclient");
+
+        LineCount lineCount = jloc.countLines();
+
+        assertEquals(24, lineCount.getFiles());
+        assertEquals(373, lineCount.getLines());
     }
 }
