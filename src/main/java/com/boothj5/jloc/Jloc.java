6e1333265aab0fcaabf8c16d0ebae6eb01edd640
James Booth <boothj5@gmail.com>
2015-04-13 23:05:31 +0100

    Removed old files

--- src/main/java/com/boothj5/jloc/Jloc.java
+++ src/main/java/com/boothj5/jloc/Jloc.java
@@ -1,11 +1,6 @@
 package com.boothj5.jloc;
 
-import java.io.File;
-import java.util.List;
-
 public class Jloc {
-
-    private static final List<LangDef> langDefs = Configuration.getDefs();
 
     public Jloc(String directory) {
 
@@ -22,18 +17,4 @@ public class Jloc {
     public CodeCount countCode() {
         return null;
     }
-
-//    public static void main(String[] args) {
-//        for (LangDef langDef : langDefs) {
-//            System.out.println("Analysing " + langDef.getName());
-//            FileLister fileLister = new FileLister("/home/james/projects-git/mosaic", langDef.getExtensions());
-//
-//            List<File> files = fileLister.getFileListing();
-//            if (files == null) {
-//                System.out.println("No code found.");
-//            } else {
-//                System.out.println("Num files:" + files.size());
-//            }
-//        }
-//    }
 }
