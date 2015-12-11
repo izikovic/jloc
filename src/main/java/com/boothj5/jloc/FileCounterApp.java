09f6a5461350bed409745dd28906ee25188ebaf7
Ivan Žiković <izikovic@gmail.com>
2015-12-11 16:10:06 +0100

    i.z. - fix

--- src/main/java/com/boothj5/jloc/FileCounterApp.java
+++ src/main/java/com/boothj5/jloc/FileCounterApp.java
@@ -7,36 +7,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileCounterApp {
-	public static List<File> getAllFiles(String path) {
+	public static List<File> getAllFiles(String path, String extension) {
 		List<File> files = new ArrayList<>();
-		
+
 		File[] currentFiles = new File(path).listFiles();
-		
+
 		for (File file : currentFiles) {
-			if (file.isDirectory())
-				files.addAll(getAllFiles(file.getAbsolutePath()));
-			else
+			if (file.isDirectory()) {
+				files.addAll(getAllFiles(file.getAbsolutePath(), extension));
+			} else {
+				if (file.getAbsolutePath().toUpperCase().endsWith(extension.toUpperCase())) {
 				files.add(file);
+				}
+			}
+
 		}
-		
+
 		return files;
 	}
 	
-	public static int countFiles(String path) {		
+	public static List<File> getAllFiles(String path) {
+		return getAllFiles(path, "");
+	}
+
+	public static int countFiles(String path) {
 		return getAllFiles(path).size();
 	}
-	
+
 	public static void main(String[] args) {
 		String path = "";
 		if (args.length == 0) {
 			Path currentRelativePath = Paths.get("");
 			path = currentRelativePath.toAbsolutePath().toString();
 		} else {
-			path = args[0];			
+			path = args[0];
 		}
-		
-		int result = countFiles(path);		
-		
+
+		int result = countFiles(path);
+
 		System.out.println("Files: " + result);
 	}
 }
